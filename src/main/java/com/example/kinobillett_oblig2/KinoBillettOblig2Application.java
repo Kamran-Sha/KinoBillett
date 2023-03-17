1c3d5c17e8bd6b7deeb5d0db00409d873fcbca98
kamran sha <kamran-sha@hotmail.com>
2023-03-17 15:19:06 +0100

    Initial commit

---
+++ src/main/java/com/example/kinobillett_oblig2/KinoBillettOblig2Application.java
@@ -0,0 +1,13 @@
+package com.example.kinobillett_oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoBillettOblig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoBillettOblig2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kinobillett_oblig2/KinoBillettOblig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinobillett_oblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBillettOblig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
